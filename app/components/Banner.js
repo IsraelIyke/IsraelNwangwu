548cfbdb507dfcaa2c08fded82950972ecb125bc
IsraelIyke <nwangwuisrael@gmail.com>
2023-12-04 23:31:40 +0100

    added new cv

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -14,13 +14,26 @@ export default function Banner() {
           creativity meets functionality. Let&quot;s collaborate and craft
           something extraordinary together.
         </div>
-        <div className="banner-cta-btn">
-          <a href="mailto:nwangwujeremy@gmail.com">
-            <button className="banner-hire-me">Hire Me</button>
-          </a>
-          <a href="/CV/Nwangwu-Israel-CV.pdf" download="Nwangwu-Israel-CV.pdf">
-            <button className="banner-resume">Resume</button>
-          </a>
+        <div className=" flex flex-col items-start">
+          <div className="banner-cta-btn">
+            <a
+              href="/images/cert.png"
+              download="Nwangwu-Israel-Certification.png"
+            >
+              <button className="banner-resume">Certification</button>
+            </a>
+            <a
+              href="/CV/Nwangwu-Israel-CV.pdf"
+              download="Nwangwu-Israel-CV.pdf"
+            >
+              <button className="banner-resume ml-[1rem]">Resume</button>
+            </a>
+          </div>
+          <div className="">
+            <a href="mailto:nwangwujeremy@gmail.com">
+              <button className="banner-hire-me">Hire Me</button>
+            </a>
+          </div>
         </div>
       </div>
       <div className="banner-image-container">
